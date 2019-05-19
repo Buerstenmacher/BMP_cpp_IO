6dabe920c092d02dece9596830bd3b46329a7915
Buerstenmacher <roman_lechner@yahoo.com>
2019-05-19 21:34:54 +0200

    -

--- bmp.h
+++ bmp.h
@@ -7,111 +7,108 @@
 #include <string.h>
 #include <iostream>
 
-struct RGB
-{
-	unsigned char blue = 0xFF;
-	unsigned char green = 0xFF;
-	unsigned char red = 0xFF;
-	RGB(int rgb) {
-		red = (rgb & 0xFF0000) >> 16;
-		green = (rgb & 0x00FF00) >> 8;
-		blue = (rgb & 0x0000FF);
-	}
-	RGB(unsigned char r, unsigned char g, unsigned char b) {
-		red = r;
-		green = g;
-		blue = b;
-	}
-	RGB() {
-
-	}
-	bool operator==(const RGB& b) {
-		if (this->blue == b.blue && this->green == b.green && this->red == b.red)
-			return true;
-		else
-			return false;
-	}
-};
+struct RGB {
+uint8_t blue,green,red;
+RGB(uint32_t rgb):blue{uint8_t((rgb & 0x0000FF) >> 0)},green{uint8_t((rgb & 0x00FF00) >> 8)},red{uint8_t((rgb & 0xFF0000) >> 16)} {}
+RGB(uint8_t r, uint8_t g, uint8_t b):blue{b},green{g},red{r} {}
+RGB():blue{255},green{255},red{255} {}
+bool operator==(const RGB& b) {return (this->blue == b.blue && this->green == b.green && this->red == b.red)?true:false;}
+};//struct RGB
 
 struct DIB_header {
-	unsigned int header_size = 40; // DIB_header's size is determined by its type. 0x0E/14, 32 bits.
-	int width; // the bitmap width in pixels (signed integer). 0x12/18, 32 bits.
-	int height; // the bitmap height in pixels (signed integer). 0x16/22, 32 bits.
-	unsigned short color_planes = 1; // the number of color planes (must be 1). 0x1A/26, 16 bits.
-	unsigned short bits_per_pixel = 24; // the number of bits per pixel, which is the color depth of the image. Typical values are 1, 4, 8, 16, 24 and 32. 0x1C/28, 16 bits.
-	unsigned int compression_method = 0; // the compression method being used. 0x1E/30, 32 bits. 	
-	unsigned int raw_data_size = 0; // the image size. This is the size of the raw bitmap data; a dummy 0 can be given for BI_RGB bitmaps. 0x22/34, 32 bits.
-	int h_reso = 5669; // the horizontal resolution of the image. (pixel per metre, signed integer) 0x26/38, 32 bits. 	
-	int v_reso = 5669; // the vertical resolution of the image. (pixel per metre, signed integer) 0x2A/42, 32 bits.
-	unsigned int colors = 0; // the number of colors in the color palette, or 0 to default to 2^n. 0x2E/46, 32 bits.
-	unsigned int important_colors = 0; // the number of important colors used, or 0 when every color is important; generally ignored. 0x32/50, 32 bits.
-};
+uint32_t header_size = 40; // DIB_header's size is determined by its type. 0x0E/14, 32 bits.
+int32_t width; // the bitmap width in pixels (signed integer). 0x12/18, 32 bits.
+int32_t height; // the bitmap height in pixels (signed integer). 0x16/22, 32 bits.
+uint16_t color_planes = 1; // the number of color planes (must be 1). 0x1A/26, 16 bits.
+uint16_t bits_per_pixel = 24; // the number of bits per pixel, which is the color depth of the image. Typical values are 1, 4, 8, 16, 24 and 32. 0x1C/28, 16 bits.
+uint32_t compression_method = 0; // the compression method being used. 0x1E/30, 32 bits.
+uint32_t raw_data_size = 0; // the image size. This is the size of the raw bitmap data; a dummy 0 can be given for BI_RGB bitmaps. 0x22/34, 32 bits.
+int32_t h_reso = 5669; // the horizontal resolution of the image. (pixel per metre, signed integer) 0x26/38, 32 bits.
+int32_t v_reso = 5669; // the vertical resolution of the image. (pixel per metre, signed integer) 0x2A/42, 32 bits.
+uint32_t colors = 0; // the number of colors in the color palette, or 0 to default to 2^n. 0x2E/46, 32 bits.
+uint32_t important_colors = 0; // the number of important colors used, or 0 when every color is important; generally ignored. 0x32/50, 32 bits.
+};//struct DIB_header
 
-struct BMP_header
-{
-	// BMP header's size is fixed, which equals to 14 bits. 
-	char head[3] = { 'B','M','\0' };
-	unsigned int file_size;
-	unsigned int reserved_field = 0;
-	unsigned int starting_addr = 54;
-	unsigned int DIB_header_size = 40;
-	DIB_header DIB;
-	operator bool() {
-		if (strcmp(head, "BM") == 0 || strcmp(head, "BA") == 0 || strcmp(head, "CI") == 0 || strcmp(head, "CP") == 0 || strcmp(head, "IC") == 0 || strcmp(head, "PT") == 0)
-			return true;
-		else
-			return false;
-	}
-};
+struct BMP_header {// BMP header's size is fixed, which equals to 14 bits.
+char head[3] = { 'B','M','\0' };
+uint32_t file_size;
+uint32_t reserved_field = 0;
+uint32_t starting_addr = 54;
+uint32_t DIB_header_size = 40;
+DIB_header DIB;
+operator bool() {return (strcmp(head, "BM") == 0 || strcmp(head, "BA") == 0 || strcmp(head, "CI") == 0 || strcmp(head, "CP") == 0 || strcmp(head, "IC") == 0 || strcmp(head, "PT") == 0)?true:false;}
+};//struct BMP_header
 
 BMP_header check_BMP_file(FILE* bmpfile) {
-	BMP_header ret;
-	fread(ret.head, 1, 2, bmpfile);
-	fread(&ret.file_size, 4, 1, bmpfile);
-	fread(&ret.reserved_field, 4, 1, bmpfile);
-	fread(&ret.starting_addr, 4, 1, bmpfile);
-	fread(&ret.DIB_header_size, 4, 1, bmpfile);
-	if (ret.DIB_header_size == 40) {
-		ret.DIB.header_size = ret.DIB_header_size;
-		fread(&ret.DIB.width, 4, 1, bmpfile);
-		fread(&ret.DIB.height, 4, 1, bmpfile);
-		fread(&ret.DIB.color_planes, 2, 1, bmpfile);
-		fread(&ret.DIB.bits_per_pixel, 2, 1, bmpfile);
-		fread(&ret.DIB.compression_method, 4, 1, bmpfile);
-		fread(&ret.DIB.raw_data_size, 4, 1, bmpfile);
-		fread(&ret.DIB.h_reso, 4, 1, bmpfile);
-		fread(&ret.DIB.v_reso, 4, 1, bmpfile);
-		fread(&ret.DIB.colors, 4, 1, bmpfile);
-		fread(&ret.DIB.important_colors, 4, 1, bmpfile);
-	}
-	rewind(bmpfile);
-	return ret;
+BMP_header ret;
+fread(ret.head, 1, 2, bmpfile);
+fread(&ret.file_size, 4, 1, bmpfile);
+fread(&ret.reserved_field, 4, 1, bmpfile);
+fread(&ret.starting_addr, 4, 1, bmpfile);
+fread(&ret.DIB_header_size, 4, 1, bmpfile);
+if (ret.DIB_header_size == 40) {
+	ret.DIB.header_size = ret.DIB_header_size;
+	fread(&ret.DIB.width, 4, 1, bmpfile);
+	fread(&ret.DIB.height, 4, 1, bmpfile);
+	fread(&ret.DIB.color_planes, 2, 1, bmpfile);
+	fread(&ret.DIB.bits_per_pixel, 2, 1, bmpfile);
+	fread(&ret.DIB.compression_method, 4, 1, bmpfile);
+	fread(&ret.DIB.raw_data_size, 4, 1, bmpfile);
+	fread(&ret.DIB.h_reso, 4, 1, bmpfile);
+	fread(&ret.DIB.v_reso, 4, 1, bmpfile);
+	fread(&ret.DIB.colors, 4, 1, bmpfile);
+	fread(&ret.DIB.important_colors, 4, 1, bmpfile);
+}
+rewind(bmpfile);
+return ret;
 }
 
-int get_row_size(BMP_header h) {
-	return ((h.DIB.bits_per_pixel*h.DIB.width + 31) / 32) * 4;
-}
-
-int get_data_size(BMP_header h) {
-	return get_row_size(h)*abs(h.DIB.height);
-}
+int get_row_size(BMP_header h) {return ((h.DIB.bits_per_pixel*h.DIB.width + 31) / 32) * 4;}
+int get_data_size(BMP_header h) {return get_row_size(h)*abs(h.DIB.height);}
 
 struct Point {
-	int x = 0;
-	int y = 0;
-	RGB color;
-	Point(int x, int y, RGB rgb) : x(x), y(y), color(rgb) { }
-	Point() {}
-};
+int32_t x,y;
+RGB color;
+Point(int32_t x, int32_t y, RGB rgb) : x(x), y(y), color(rgb) {}
+Point():x{0},y{0},color{} {}
+};//struct Point
 
 class BMP{
-	FILE* bmpfile;
-	int row_size;
+FILE* bmpfile;
+int row_size;
+BMP_header header;
+const int& width = header.DIB.width;
+const int& height = header.DIB.height;
+RGB* data;
+
+	void fill_(Point p, const RGB& origin) {
+		if (p.x < width && p.y < height && data[p.y * width + p.x] == origin) {
+			this->put_pixel(p);
+			Point up = p;
+			Point down = p;
+			Point left = p;
+			Point right = p;
+			up.y--;
+			down.y++;
+			left.x--;
+			right.x++;
+			if (up.x < width && up.y < height && up.y >= 0 && up.x >= 0 && data[up.y * width + up.x] == origin) {
+				fill_(up, origin);
+			}
+			if (down.x < width && down.y < height && down.y >= 0 && down.x >= 0 && data[down.y * width + down.x] == origin) {
+				fill_(down, origin);
+			}
+			if (left.x < width && left.y < height && left.y >= 0 && left.x >= 0 && data[left.y * width + left.x] == origin) {
+				fill_(left, origin);
+			}
+			if (right.x < width && right.y < height && right.y >= 0 && right.x >= 0 && data[right.y * width + right.x] == origin) {
+				fill_(right, origin);
+			}
+		}
+	}
+
+
 public:
-	BMP_header header;
-	const int& width = header.DIB.width;
-	const int& height = header.DIB.height;
-	RGB* data;
 	BMP(int width, int height, const char* path) {
 		header.DIB.width = width;
 		header.DIB.height = height;
@@ -143,6 +140,9 @@ public:
 		fclose(bmpfile);
 		delete[] data;
 	}
+BMP(const BMP&) = delete;
+BMP operator=(const BMP&)=delete;
+
 	void print() {
 		for (int i = 0; i < height; i++) {
 			for (int j = 0; j < width; j++) {
@@ -267,31 +267,6 @@ public:
 	}
 
 private:
-	void fill_(Point p, const RGB& origin) {
-		if (p.x < width && p.y < height && data[p.y * width + p.x] == origin) {
-			this->put_pixel(p);
-			Point up = p;
-			Point down = p;
-			Point left = p;
-			Point right = p;
-			up.y--;
-			down.y++;
-			left.x--;
-			right.x++;
-			if (up.x < width && up.y < height && up.y >= 0 && up.x >= 0 && data[up.y * width + up.x] == origin) {
-				fill_(up, origin);
-			}
-			if (down.x < width && down.y < height && down.y >= 0 && down.x >= 0 && data[down.y * width + down.x] == origin) {
-				fill_(down, origin);
-			}
-			if (left.x < width && left.y < height && left.y >= 0 && left.x >= 0 && data[left.y * width + left.x] == origin) {
-				fill_(left, origin);
-			}
-			if (right.x < width && right.y < height && right.y >= 0 && right.x >= 0 && data[right.y * width + right.x] == origin) {
-				fill_(right, origin);
-			}
-		}
-	}
 };
 
 #endif // !BMP_H
